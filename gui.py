462e551337cb8867ec43bd8b8820698292dd943e
Bloomy <bloomyxdkox@gmail.com>
2024-03-07 09:48:52 +0100

    Dodanie pyperclip, zapisywanie w json

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import main
+import json
 
 
 window = tk.Tk()
@@ -11,17 +12,37 @@ window.config(padx=20, pady=20)
 
 def dodajBut():
 
+    strona = webEnt.get()
+    mail = emaEnt.get()
+    password = pasEnt.get()
+    dataaa = {
+        strona: {
+        'email': mail,
+        'password': password
+        }
+    }
     if pasEnt.get() == '' or webEnt == '':
         messagebox.showinfo(title='Błąd', message='Zostawiłeś/aś wolne pole!')
     else:
-        message = messagebox.askokcancel(title=f'{webEnt.get()}',
-                                         message=f'Twoje informacje: \nEmail: {emaEnt.get()} \nHasło: {pasEnt.get()}'
-                                                 f'\nCzy chcesz zapisać te informacje?')
-        if message:
-            with open('data.txt', 'a') as plik:
-                plik.write(f'{webEnt.get()} | {emaEnt.get()} | {pasEnt.get()} \n')
+        try:
+            with open('data.json', 'r') as plik:
+
+                dataa = json.load(plik)
+                dataa.update(dataaa)
+
+        except FileNotFoundError:
+            with open('data.json', 'w') as plik:
+                json.dump(dataaa, plik, indent=4)
+
+        else:
+            with open('data.json', 'w') as plik:
+
+                json.dump(dataa, plik, indent=4)
+
+        finally:
                 webEnt.delete(0, 'end')
                 pasEnt.delete(0, 'end')
+
 
 
 # ---------------- BUDOWA GUI -------------- #
@@ -41,19 +62,22 @@ email.grid(row=2, column=0)
 haslo = tk.Label(text='Hasło:')
 haslo.grid(row=3, column=0)
 
-webEnt = tk.Entry(width=48)
+webEnt = tk.Entry(width=33)
 webEnt.focus()
-webEnt.grid(row=1, column=1, columnspan = 2)
+webEnt.grid(row=1, column=1)
 
 emaEnt = tk.Entry(width=48)
 emaEnt.grid(row=2, column=1, columnspan = 2)
-emaEnt.insert(0, 'twójmail@gmail.com')
+emaEnt.insert(0, 'twojmail@gmail.com')
 
 pasEnt = tk.Entry(width=33)
 pasEnt.grid(row=3, column=1)
 
 genPass = tk.Button(text='Generuj Hasło', command=main.hasloo)
 genPass.grid(row=3, column= 2)
+
+search = tk.Button(text='Szukaj', width=11)
+search.grid(row=1, column=2)
 
 addButt = tk.Button(text='Dodaj', width=40, command=dodajBut)
 addButt.grid(row=4, column=1, columnspan=2)
